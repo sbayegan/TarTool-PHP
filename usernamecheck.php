3f62a6e2202602ae5e2b40537458874aa29dd1e9
sbayegan <sbayegan@users.noreply.github.com>
2015-04-27 15:19:24 -0400

    Update usernamecheck.php

--- usernamecheck.php
+++ usernamecheck.php
@@ -10,5 +10,7 @@ $username = mysql_real_escape_string($POST['username']); // Some clean up ***
 
 $check_for_username = conn->query("SELECT USERNAME FROM USERS WHERE USERNAME='$username'");//query to check if the username is taken
 if(mysql_num_rows($check_for_username)){echo '1';} // If there is a record match return one
-else{echo '0';}}// No record found
+else{echo '0';}
+	
+}// No record found
 ?>
