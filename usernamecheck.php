633e12c4ac87e78fb7e9c78043f696de206450c5
root <root@ip-172-31-42-68.us-west-2.compute.internal>
2015-04-27 20:29:08 +0000

    usernamecheck should be working

--- usernamecheck.php
+++ usernamecheck.php
@@ -1,14 +1,22 @@
 <?php
 
-include ('datalogin.php');// Connect to the database - Object 'conn' is created for
+include('datalogin.php');// Connect to the database - Object 'conn' is created for
 			// communication, remember to close it
 
-
-if(isset($POST['username'])) //If a username has been submitted
+if(isset($_POST['username'])) //If a username has been submitted
 {
-$username = mysql_real_escape_string($POST['username']); // Some clean up ***
 
-$check_for_username = conn->query("SELECT USERNAME FROM USERS WHERE USERNAME='$username'");//query to check if the username is taken
-if(mysql_num_rows($check_for_username)){echo '1';} // If there is a record match return one
-else{echo '0';}}// No record found
+
+$username = mysql_real_escape_string($_POST['username']); // Some clean up ***
+
+$result = $conn->query("SELECT USERNAME FROM USERS WHERE USERNAME='$username'");//query to check if the username is taken
+//$result = $conn->query("SELECT * FROM LOCAS");//query to check if the username is taken
+$rows = $result->num_rows ;
+if($rows){echo '1';} // If there is a record match return one
+		   else    {echo '0';}// No record found
+}
+
+$conn->close();
+
 ?>
+
