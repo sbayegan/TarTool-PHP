b458e6091f0002ef8d3a8c0e7819957bf9ef5919
root <root@ip-172-31-42-68.us-west-2.compute.internal>
2015-04-27 22:16:33 +0000

    added emailcheck.php

---
+++ emailcheck.php
@@ -0,0 +1,24 @@
+<?php
+
+include('datalogin.php');// Connect to the database - Object 'conn' is created for
+			// communication, remember to close it
+
+if(isset($_GET['username'])) //If a username has been submitted
+{
+
+
+
+$username = ($_GET['username']); // Some clean up ***
+
+
+$result = $conn->query("SELECT USERNAME FROM USERS WHERE USERNAME='$username'");//query to check if the username is taken
+
+$rows = $result->num_rows ;
+if($rows){echo '0';} // If there is a record match return one
+		   else    {echo '1';}// No record found
+}
+
+$conn->close();
+
+?>
+
