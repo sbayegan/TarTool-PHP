cb849df1459c36afa235fcd2c412c3ef141e0000
root <root@ip-172-31-42-68.us-west-2.compute.internal>
2015-04-27 19:18:25 +0000

    added stuff

---
+++ datalogin.php
@@ -0,0 +1,13 @@
+
+<?php 
+
+
+$servername = 'locations.cnesdpnjh1yy.us-west-2.rds.amazonaws.com';
+$username = 'yell';
+$password = 'Toronto1';
+$db = 'yell';
+$conn = new mysqli($servername, $username, $password, $db);
+if($conn->connect_error){die("Connection failed: " . $conn->connect_error);}
+
+//$conn->close();
+?>
---
+++ usernamecheck.php
@@ -0,0 +1,14 @@
+<?php
+
+include ('datalogin.php');// Connect to the database - Object 'conn' is created for
+			// communication, remember to close it
+
+
+if(isset($POST['username'])) //If a username has been submitted
+{
+$username = mysql_real_escape_string($POST['username']); // Some clean up ***
+
+$check_for_username = conn->query("SELECT USERNAME FROM USERS WHERE USERNAME='$username'");//query to check if the username is taken
+if(mysql_num_rows($check_for_username)){echo '1';} // If there is a record match return one
+else{echo '0';}}// No record found
+?>
