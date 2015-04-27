958b77e6b7df45da10471923de82c470c53017c2
sbayegan <sbayegan@users.noreply.github.com>
2015-04-27 18:18:13 -0400

    Update emailcheck.php

--- emailcheck.php
+++ emailcheck.php
@@ -3,19 +3,14 @@
 include('datalogin.php');// Connect to the database - Object 'conn' is created for
 			// communication, remember to close it
 
-if(isset($_GET['username'])) //If a username has been submitted
+if(isset($_GET['email'])) //If a username has been submitted
 {
 
-
-
-$username = ($_GET['username']); // Some clean up ***
-
-
-$result = $conn->query("SELECT USERNAME FROM USERS WHERE USERNAME='$username'");//query to check if the username is taken
-
-$rows = $result->num_rows ;
-if($rows){echo '0';} // If there is a record match return one
-		   else    {echo '1';}// No record found
+$email = test_input($_POST["email"]);
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+  echo '0'; 
+}
+else{echo '1';}
 }
 
 $conn->close();
