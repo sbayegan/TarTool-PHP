83a4e278b703dcfe3e332d0c005736e318103a1f
sbayegan <sbayegan@users.noreply.github.com>
2015-04-28 01:24:16 -0400

    Update passwordcheck.php

--- passwordcheck.php
+++ passwordcheck.php
@@ -1,9 +1,5 @@
 <?php
 
-/*
- * PHP PCRE - How to validate complex passwords using regular expressions
- */
-
 if(isset($_POST['pass'])) //If a username has been submitted
 {
 $pass = $_POST['pass'];  
@@ -12,23 +8,11 @@ $pass = $_POST['pass'];
    {
    echo '1';
    }
-   else
+   else{
    echo '0';
-   
+   }
 }
 
 
-/*
-    Explaining $\S*(?=\S{8,})(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[\d])(?=\S*[\W])\S*$
-    $ = beginning of string
-    \S* = any set of characters
-    (?=\S{8,}) = of at least length 8
-    (?=\S*[a-z]) = containing at least one lowercase letter
-    (?=\S*[A-Z]) = and at least one uppercase letter
-    (?=\S*[\d]) = and at least one number
-    (?=\S*[\W]) = and at least a special character (non-word characters)
-    $ = end of the string
-
- */
 ?>
 
