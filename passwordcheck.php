812a8f4d89f8260aaedd10660253cead02c2454f
sbayegan <sbayegan@users.noreply.github.com>
2015-04-28 01:32:20 -0400

    Update passwordcheck.php

--- passwordcheck.php
+++ passwordcheck.php
@@ -1,22 +1,15 @@
 <?php
 
-function valid_pass($candidate) {
-    if (!preg_match_all('$\S*(?=\S{8,})(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[\d])(?=\S*[\W])\S*$', $candidate))
-        return FALSE;
-    return TRUE;
-}
 
 if(isset($_POST['pass'])) //If a username has been submitted
 {
 $pass = $_POST['pass'];  
 
-    if(valid_pass($pass))
-   {
-   echo '1';
-   }
-   else{
-   echo '0';
-   }
+    if (preg_match('/^(?=[a-z])(?=[A-Z])[a-zA-Z]{8,}$/', $pass))
+{
+    echo '1';
+}
+else {echo '0';}
 }
 
 
