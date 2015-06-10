f69e0512f91a7c9d68c0d9d2edace4afb66638eb
sbayegan <sbayegan@users.noreply.github.com>
2015-06-10 01:06:56 -0400

    Update home.php

--- home.php
+++ home.php
@@ -40,14 +40,29 @@ function favorite(cardid) {
 echo '
 <script>
 function unfavorite(cardid) {
-    id = "save-";
-    id += cardid;
-    //document.getElementById("XXX").innerHTML = id;
-     output = "<span onclick=\'favorite(";
-     output += cardid ;
-     output += ")\'    > <img src=\'http://junto.link/pictures/save.png\'  width=\'55\' height=\'55\' style=\'float:left;margin-left:0px;margin-top:15px\'></span>";
- 
-    document.getElementById(id).innerHTML= output;}
+  var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                if(xmlhttp.responseText == 1){
+    			id = "save-";
+			id += cardid;
+    			//document.getElementById("XXX").innerHTML = id;
+        		output = "<span onclick=\'favorite(";
+        		output += cardid ;
+        		output += ")\'    > <img src=\'http://junto.link/pictures/save.png\'  width=\'55\' height=\'55\' style=\'float:left;margin-left:0px;margin-top:15px\'></span>";
+        		document.getElementById(id).innerHTML= output;
+                			}
+
+     		}
+   	}         
+
+        xmlhttp.open("POST", "unfavorite.php", true);
+        xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+        xmlhttp.send("id="+cardid);
+                	
+                	
+                	
+                }
 	</script>';
 
 
