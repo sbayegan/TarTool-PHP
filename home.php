f72d9b570f7aeeea973c1dfb42d939f6b8dadcb4
sbayegan <sbayegan@users.noreply.github.com>
2015-06-10 01:59:11 -0400

    Update home.php

--- home.php
+++ home.php
@@ -26,15 +26,26 @@ function favorite(cardid) {
                       output += ")\'  > <img src=\'http://junto.link/pictures/cross-red.png\'  width=\'55\' height=\'55\' style=\'float:left;margin-left:0px;margin-top:15px\'></span>"
                       document.getElementById(id).innerHTML= output;
                     }
-
      }
    }         
-
         xmlhttp.open("POST", "favorite.php", true);
         xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
         xmlhttp.send("id="+cardid);
-
-
+}
+function approve(cardid) {
+   var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                if(xmlhttp.responseText == 1){
+	              id = "approve-";
+	              id += cardid;
+                      document.getElementById(id).innerHTML= "";
+                    }
+     }
+   }         
+        xmlhttp.open("POST", "approve.php", true);
+        xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+        xmlhttp.send("id="+cardid);
 }
 	</script>';
 echo '
