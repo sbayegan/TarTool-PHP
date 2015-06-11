c0d18bd73a2817d5b34cba2112d6c35957629c64
sbayegan <sbayegan@users.noreply.github.com>
2015-06-11 18:59:25 -0400

    Update code.js

--- JS/code.js
+++ JS/code.js
@@ -34,3 +34,23 @@ function approve(cardid) {
         xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
         xmlhttp.send("id="+cardid);
 }
+function unfavorite(cardid) {
+  var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                if(xmlhttp.responseText == 1){
+    			id = "save-";
+			id += cardid;
+    			//document.getElementById("XXX").innerHTML = id;
+        		output = "<span onclick=\'favorite(";
+        		output += cardid ;
+        		output += ")\'    > <img src=\'http://junto.link/pictures/save.png\'  width=\'55\' height=\'55\' style=\'float:left;margin-left:0px;margin-top:15px\'></span>";
+        		document.getElementById(id).innerHTML= output;
+                			}
+     								}
+   					        }         
+
+        xmlhttp.open("POST", "unfavorite.php", true);
+        xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+        xmlhttp.send("id="+cardid);
+                }
