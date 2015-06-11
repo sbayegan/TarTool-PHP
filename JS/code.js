9b79869fb821daea57991e4bd76fdbddab122c6e
sbayegan <sbayegan@users.noreply.github.com>
2015-06-11 19:02:27 -0400

    Update code.js

--- JS/code.js
+++ JS/code.js
@@ -54,3 +54,320 @@ function unfavorite(cardid) {
         xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
         xmlhttp.send("id="+cardid);
                 }
+                
+var lastused = 2;
+var currentcat = "BD"
+function activate(str){
+if(str.length > 5){document.getElementById("submit_bt").disabled = false;}
+else {document.getElementById("submit_bt").disabled = true;}
+}
+function update(str){
+lastused = 2;
+document.getElementById("adderbutton").disabled = false;
+if (document.getElementById("D5")){var element = document.getElementById("D5");element.parentNode.removeChild(element);}
+if (document.getElementById("D4")){var element = document.getElementById("D4");element.parentNode.removeChild(element);}
+if (document.getElementById("D3")){var element = document.getElementById("D3");element.parentNode.removeChild(element);}
+if (document.getElementById("D2")){var element = document.getElementById("D2");element.parentNode.removeChild(element);}
+currentcat = str;
+if(str == "FE"){document.getElementById("D1").innerHTML = 
+	\'<option value="">Choose One</option>\'+
+        \'<option value="UserInterface">User Interface</option>\'+
+        \'<option value="UserExperience">User Experience</option>\'+
+        \'<option value="MockupsAndWireframing">Mockups & Wireframing</option>\'+
+        \'<option value="HTML">HTML</option>\'+
+        \'<option value="CSS">CSS</option>\'+
+        \'<option value="JavaScript">JavaScript</option>\'+
+        \'<option value="Themes">Themes</option>\'+
+        \'<option value="Mobile">Mobile</option>\'+
+        \'<option value="FrontEndiOS">iOS</option>\'+
+        \'<option value="FrontEndAndroid">Android</option>\'+
+        \'<option value="Bootstrap">Bootstrap</option>\'+
+        \'<option value="XML">XML</option>\'+
+	\'<option value="JQuery">jQuery</option>\'+
+	\'<option value="Angular">Angular</option>\'+
+	\'<option value="Canvas">Canvas</option>\'+
+	\'<option value="SVG">Scalable Vector Graphics</option>\'+
+	\'<option value="JSON">JSON</option>\'+
+        \'<option value="Ajax">Ajax</option>\';
+}
+if(str == "BE"){document.getElementById("D1").innerHTML = 
+	\'<option value="">Choose One</option>\'+
+        \'<option value="Security">Security</option>\'+
+        \'<option value="DataManagement">Data Management</option>\'+
+        \'<option value="Hosting">Hosting</option>\'+
+        \'<option value="PHP">PHP</option>\'+
+        \'<option value="Python">Python</option>\'+
+	\'<option value="ASPNET">ASP.NET</option>\'+
+	\'<option value="VBScript">Visual Basic Script</option>\'+
+	\'<option value="SQL">SQL</option>\'+
+	\'<option value="C">C</option>\'+
+	\'<option value="C++">C++</option>\'+
+	\'<option value="Shell">Shell</option>\'+
+	\'<option value="Java">Java</option>\'+
+	\'<option value="Ruby">Ruby</option>\'+
+	\'<option value="Objective-C">Objective-C</option>\'+
+	\'<option value="Swift">Swift</option>\'+
+        \'<option value="C#">C#</option>\'+
+        \'<option value="Debugging Tools">Debugging</option>\';
+}
+if(str == "BD"){document.getElementById("D1").innerHTML = 
+	\'<option value="">Choose One</option>\'+
+	\'<option value="LeanStartup">Lean Startup</option>\'+
+        \'<option value="MarketingAndResearch">Marketing & Research</option>\'+
+        \'<option value="Naming">Naming</option>\'+
+        \'<option value="CopyWriting">Copywriting</option>\'+
+        \'<option value="Analytics">Analytics</option> \'+
+        \'<option value="Launching">Launching</option>\'+
+	\'<option value="UserFeedback">User Feedback</option>\'+        
+        \'<option value="SEO">SEO</option>\'+
+        \'<option value="SocialMediaCommunity">Social Media & Community</option>\'+
+        \'<option value="ProjectManagement">Project Management</option>\'+
+        \'<option value="CustomerService">Customer Service</option>\'+
+        \'<option value="InventoryManagement">Inventory Management</option>\'+
+        \'<option value="Sales">Sales</option>\'+
+	\'<option value="Funding">Funding</option>\'+
+	\'<option value="Administration">Administration</option>\'+
+	\'<option value="Productivity">Productivity</option>\'+
+        \'<option value="Outsourcing">Outsourcing</option>\'+
+        \'<option value="E-commerce">E-commerce</option>\'+
+	\'<option value="AcceleratorsAndIncubators">Accelerators & Incubators</option>\'+
+	\'<option value="Events">Events</option>\';
+}
+}
+function add() {
+var FE = 
+	\'<select name="subcat\' + lastused + \'" form="form" class="form-control" id=lastused>\'+
+	\'<option value="">Choose One</option>\'+
+        \'<option value="UserInterface">User Interface</option>\'+
+        \'<option value="UserExperience">User Experience</option>\'+
+        \'<option value="MockupsAndWireframing">Mockups & Wireframing</option>\'+
+        \'<option value="HTML">HTML</option>\'+
+        \'<option value="CSS">CSS</option>\'+
+        \'<option value="JavaScript">JavaScript</option>\'+
+        \'<option value="Themes">Themes</option>\'+
+        \'<option value="Mobile">Mobile</option>\'+
+        \'<option value="FrontEndiOS">iOS</option>\'+
+        \'<option value="FrontEndAndroid">Android</option>\'+
+        \'<option value="Bootstrap">Bootstrap</option>\'+
+        \'<option value="XML">XML</option>\'+
+	\'<option value="JQuery">jQuery</option>\'+
+	\'<option value="Angular">Angular</option>\'+
+	\'<option value="Canvas">Canvas</option>\'+
+	\'<option value="SVG">Scalable Vector Graphics</option>\'+
+	\'<option value="JSON">JSON</option>\'+
+        \'<option value="Ajax">Ajax</option>\'+
+	\'</select>\';
+var BE = 
+	\'<select name="subcat\'+ lastused +\'" form="form" class="form-control" id=lastused>\'+
+	\'<option value="">Choose One</option>\'+
+        \'<option value="Security">Security</option>\'+
+        \'<option value="DataManagement">Data Management</option>\'+
+        \'<option value="Hosting">Hosting</option>\'+
+        \'<option value="PHP">PHP</option>\'+
+        \'<option value="Python">Python</option>\'+
+	\'<option value="ASPNET">ASP.NET</option>\'+
+	\'<option value="VBScript">Visual Basic Script</option>\'+
+	\'<option value="SQL">SQL</option>\'+
+	\'<option value="C">C</option>\'+
+	\'<option value="C++">C++</option>\'+
+	\'<option value="Shell">Shell</option>\'+
+	\'<option value="Java">Java</option>\'+
+	\'<option value="Ruby">Ruby</option>\'+
+	\'<option value="Objective-C">Objective-C</option>\'+
+	\'<option value="Swift">Swift</option>\'+
+        \'<option value="C#">C#</option>\'+
+        \'<option value="Debugging Tools">Debugging</option>\'+
+	\'</select>\';
+var BD = 
+ 	\'<select name="subcat\'+ lastused + \'" form="form" class="form-control" id=lastused>\'+
+ 	\'<option value="">Choose One</option>\'+
+        \'<option value="LeanStartup">Lean Startup</option>\'+
+        \'<option value="MarketingAndResearch">Marketing & Research</option>\'+
+        \'<option value="Naming">Naming</option>\'+
+        \'<option value="CopyWriting">Copywriting</option>\'+
+        \'<option value="Analytics">Analytics</option> \'+
+        \'<option value="Launching">Launching</option>\'+
+	\'<option value="UserFeedback">User Feedback</option>\'+        
+        \'<option value="SEO">SEO</option>\'+
+        \'<option value="SocialMediaCommunity">Social Media & Community</option>\'+
+        \'<option value="ProjectManagement">Project Management</option>\'+
+        \'<option value="CustomerService">Customer Service</option>\'+
+        \'<option value="InventoryManagement">Inventory Management</option>\'+
+        \'<option value="Sales">Sales</option>\'+
+	\'<option value="Funding">Funding</option>\'+
+	\'<option value="Administration">Administration</option>\'+
+	\'<option value="Productivity">Productivity</option>\'+
+        \'<option value="Outsourcing">Outsourcing</option>\'+
+        \'<option value="E-commerce">E-commerce</option>\'+
+	\'<option value="AcceleratorsAndIncubators">Accelerators & Incubators</option>\'+
+	\'<option value="Events">Events</option>\'+
+\'</select>\' ;
+var node = document.createElement("DIV");
+node.setAttribute("id", "D"+lastused);
+document.getElementById("adder").appendChild(node);              
+if(currentcat == "BE"){document.getElementById("D"+lastused).innerHTML = BE;}
+if(currentcat == "BD"){document.getElementById("D"+lastused).innerHTML = BD;}
+if(currentcat == "FE"){document.getElementById("D"+lastused).innerHTML = FE;}
+lastused = lastused + 1;
+if (lastused == 6){
+document.getElementById("adderbutton").disabled = true;
+}
+}
+
+var username = 0;
+var email = 0;
+var pass = 0;
+var repass = 0;
+function available(str) {
+    if (str.length < 4) { 
+        document.getElementById("user-status").innerHTML = "too short";
+        username = 0;
+        {document.getElementById("submit_btn").disabled = true;}
+        return;
+    } else {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                if(xmlhttp.responseText == 1){
+                  document.getElementById("user-status").innerHTML = "okay";
+                  username = 1;
+                  if(username ==1 && email ==1 && pass == 1 && repass ==1)
+                  {document.getElementById("submit_btn").disabled = false;}
+                }
+                if(xmlhttp.responseText == 0){
+                  document.getElementById("user-status").innerHTML = "taken";
+                  username = 0;
+                  {document.getElementById("submit_btn").disabled = true;}
+                }
+            }
+        }
+        xmlhttp.open("GET", "usernamecheck.php?username=" + str, true);
+        xmlhttp.send();
+    }
+}
+
+function userlogin(str) {
+    if (str.length < 4) { 
+        document.getElementById("welcome-message").innerHTML = "too short";
+        {document.getElementById("login-password").disabled = true;}
+        return;
+    } else {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                if(xmlhttp.responseText == 1){
+                   document.getElementById("welcome-message").innerHTML = "Doesnt exist";
+                  {document.getElementById("login-password").disabled = true;}
+                }
+                if(xmlhttp.responseText == 0){
+                  document.getElementById("welcome-message").innerHTML = "Okay";
+                  {document.getElementById("login-password").disabled = false;}
+                }
+            }
+        }
+        xmlhttp.open("GET", "usernamecheck.php?username=" + str, true);
+        xmlhttp.send();
+    }
+}
+
+function valid(str) {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                if(xmlhttp.responseText == 1){
+                  document.getElementById("email-status").innerHTML = "well formed :-)";
+                  email = 1;
+                  if(username ==1 && email ==1 && pass == 1 && repass ==1)
+                  {document.getElementById("submit_btn").disabled = false;}
+                }
+                 if(xmlhttp.responseText == 0){
+                  document.getElementById("email-status").innerHTML = "ill-formed email address";
+                  email = 0;
+                  {document.getElementById("submit_btn").disabled = true;}
+                }
+		if(xmlhttp.responseText == 2){
+                  document.getElementById("email-status").innerHTML = "already registered";
+                  email = 0;
+                  {document.getElementById("submit_btn").disabled = true;}
+                }
+           }
+        }
+        
+        xmlhttp.open("GET", "emailcheck.php?email=" + str, true);
+        xmlhttp.send();
+}
+
+
+
+function passcheck(str) {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                if(xmlhttp.responseText == 1){
+                  document.getElementById("pass-status").innerHTML = "good";
+                  pass = 1;
+                  if(username ==1 && email ==1 && pass == 1 && repass ==1)
+                  {document.getElementById("submit_btn").disabled = false;}
+                }
+                if(xmlhttp.responseText == 0){
+                  document.getElementById("pass-status").innerHTML = "too short";
+                  pass = 0;
+                  {document.getElementById("submit_btn").disabled = true;}
+                }
+            }
+        }
+        xmlhttp.open("POST", "passwordcheck.php", true);
+        xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+        xmlhttp.send("pass="+str);
+}
+
+
+
+
+
+
+
+
+function login(two) {
+one = document.getElementById("loginusername").value;
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+
+		 document.getElementById("condition").innerHTML =  xmlhttp.responseText;
+
+                if(xmlhttp.responseText == 1){
+                  document.getElementById("condition").innerHTML = "good, lets go";
+		  window.location.replace("home.php");
+
+                }
+                if(xmlhttp.responseText == 0){
+                  document.getElementById("condition").innerHTML = "Waiting for the correct password";
+                 
+                }
+ 		if(xmlhttp.responseText == -1){
+                  document.getElementById("condition").innerHTML = "Not activated, Please verify your email address and retype your password";
+                 
+                }
+            }
+	
+        }
+        xmlhttp.open("POST", "login.php", true);
+        xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+        xmlhttp.send("user="+one+"&pass="+two);
+}
+
+function passmatch(){
+			if(document.getElementById("password").value == document.getElementById("password1").value){
+				document.getElementById("pass1-status").innerHTML = "matched";
+				repass = 1;
+                  		if(username ==1 && email ==1 && pass == 1 && repass ==1)
+        			{document.getElementById("submit_btn").disabled = false;}
+							}
+			if(document.getElementById("password").value != document.getElementById("password1").value){
+				document.getElementById("pass1-status").innerHTML = "not mached";
+				repass = 0;
+				{document.getElementById("submit_btn").disabled = true;}
+							}
+			}						
+
