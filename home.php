abc444de569f6ff468dcecf3c9295732e41fb633
sbayegan <sbayegan@users.noreply.github.com>
2015-06-11 18:10:54 -0400

    Update home.php

--- home.php
+++ home.php
@@ -13,25 +13,7 @@ echo "Junto Home";
 echo "</title>";
 echo '
 <script>
-function favorite(cardid) {
-   var xmlhttp = new XMLHttpRequest();
-	xmlhttp.onreadystatechange = function() {
-            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                if(xmlhttp.responseText == 1){
-	              id = "save-";
-	              id += cardid;
-	              //document.getElementById("XXX").innerHTML = id;
-	              output = "<span onclick=\'unfavorite(";
-	              output += cardid;
-                      output += ")\'  > <img src=\'http://junto.link/pictures/cross-red.png\'  width=\'55\' height=\'55\' style=\'float:left;margin-left:0px;margin-top:15px\'></span>"
-                      document.getElementById(id).innerHTML= output;
-                    }
-     }
-   }         
-        xmlhttp.open("POST", "favorite.php", true);
-        xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-        xmlhttp.send("id="+cardid);
-}
+
 function approve(cardid) {
    var xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange = function() {
