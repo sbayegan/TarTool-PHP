3bfcf5ba428c05e8f41d948a8dd70c23e66c2ae7
sbayegan <sbayegan@users.noreply.github.com>
2015-06-11 17:40:40 -0400

    Update test.php

--- test.php
+++ test.php
@@ -1,4 +1,9 @@
 <?php
+/*
+This file is used to retrieve information about out php server for diagnostics.
+When you fetch test.php on your browser all of the information about the php 
+compiler and capabalitites on the server will be shown. The most convenient way to
+see the versiona and build of out PHP library.
+*/
 phpinfo();
-echo 'hello world';
 ?>
