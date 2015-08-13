6bb48829eab5010c0937088933408d8d0e0a76ad
sbayegan <sbayegan@users.noreply.github.com>
2015-08-13 02:12:33 -0400

    Update home.php

--- home.php
+++ home.php
@@ -41,6 +41,17 @@ echo "</head>";
 
 // BODY -----------------------------------------------------------------------------------------------------------------------
 echo '<body style="background-color:white;">';
+echo 
+'<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, \'script\', \'facebook-jssdk\'));</script>';
+
+
 
 echo '<div class="stick-to-top">';
 	// LOGO
