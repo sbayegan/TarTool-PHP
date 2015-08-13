53a6988463ad8861b7be0c870ce70b4d62717bd8
sbayegan <sbayegan@users.noreply.github.com>
2015-08-12 23:50:58 -0400

    Update home.php

--- home.php
+++ home.php
@@ -29,11 +29,7 @@ echo
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-<script type="text/javascript">
-	$(document).ready(function(){
-		$("#myModal").modal(\'show\');
-	});
-</script>';
+';
 
 // Include the headers associated with jQuery
 echo 
