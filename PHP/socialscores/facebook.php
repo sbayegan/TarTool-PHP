4bda5a5b6201af51abd4f34fbf19ddc7e59d403b
sbayegan <saeidjobs@gmail.com>
2016-02-22 22:31:16 -0500

    working on social count scripts

--- PHP/socialscores/facebook.php
+++ PHP/socialscores/facebook.php
@@ -6,8 +6,8 @@
 
 
 $url = $_GET['URL'];
+	if (!preg_match("~^(?:f|ht)tps?://~i", $url)) {$url = "http://" . $url;}
 $score = 0;
-
 $counter = "http://free.sharedcount.com/url?apikey=cdf41646515ec58a3822dde01b7bb862b80cd8d8&url=".$url;
 $response = file_get_contents($counter);
 $decode = json_decode($response,true);
