bd29a7a90b1eecc51cd93d11e7b0a9026abd0969
sbayegan <saeidjobs@gmail.com>
2016-02-29 16:51:18 -0500

    organizing the files

--- PHP/account/registeration.php
+++ PHP/account/registeration.php
@@ -202,7 +202,7 @@ echo '<div class="stick-to-top">';
 
 // POWER BUTTON - Check the cookie and set the color and the link of the power button accordingly
      if(isset($_COOKIE['junto'])){
-     echo '<span style="position:absolute;right:50px;top:20px;"> <a href="logout.php"><img src="pictures/power-red.png" height="45" width="45"></a></span>';
+     echo '<span style="position:absolute;right:50px;top:20px;"> <a href="logout.php"><img src="../../pictures/power-red.png" height="45" width="45"></a></span>';
      }
      else{
      echo '<span style="position:absolute;right:50px;top:20px;"> <a href="#sign"  data-toggle="modal"><img src="pictures/power.png" height="45" width="45"></a></span>';
