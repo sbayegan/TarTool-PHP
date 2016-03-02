b0306db36dd26274a1043cb4348d387d7b2affef
sbayegan <saeidjobs@gmail.com>
2016-03-02 18:52:41 -0500

    designing the navigation bar

--- score.php
+++ score.php
@@ -23,7 +23,7 @@ if(isset($decode["GooglePlusOne"])){$google=$decode["GooglePlusOne"];}else{$goog
 $total = $facebook+$twitter+$linkedin+$score;
 
 $query = 'UPDATE RESOURCES SET '.' GOOGLEPLUS='.$google.', TWITTER='.$twitter.',FACEBOOK='.$facebook.',LINKEDIN='.$linkedin.',LIKES='.$score.',TOTALSCORE='.$total.' where RESOURCEID='.$i;
-//echo $query;
+echo $query;
 //echo $query;
 $conn->query($query);
 
