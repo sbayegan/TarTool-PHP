b8f42820d12c431a917d692ab5f855b66c309ca1
sbayegan <saeidjobs@gmail.com>
2016-03-03 13:40:22 -0500

    working on file upload

--- submit.php
+++ submit.php
@@ -54,7 +54,34 @@ $conn->query($query);
 // Put the resource in the table
 $hash = rand(0,10000000000);
 $submitter = $_COOKIE['junto'];
-$query = "INSERT INTO RESOURCES VALUES($resourceid,'$title','$description','$type','$url',0,'$hash','$profileurl',0,0,0,0,0,0,NOW(),'.$submitter.')";
+
+if(isset($_FILES["fileToUpload"]["name"])){
+  $target_dir = "thumbnails".$resourceid;
+  $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+  if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;}
+  if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;}
+  if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+    // if everything is ok, try to upload file
+    } else {
+      if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";} 
+      else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+
+  $query = "INSERT INTO RESOURCES VALUES($resourceid,'$title','$description','$type','$url',0,'$hash','$target_dir',0,0,0,0,0,0,NOW(),'.$submitter.')";
+}
+else{
+  $query = "INSERT INTO RESOURCES VALUES($resourceid,'$title','$description','$type','$url',0,'$hash','$profileurl',0,0,0,0,0,0,NOW(),'.$submitter.')";
+}
+
 $conn->query($query);
 score($resourceid);
 
