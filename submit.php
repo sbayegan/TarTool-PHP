9d87ba1a928e91711292eac03f422eb99629b19b
sbayegan <saeidjobs@gmail.com>
2016-03-03 18:02:30 -0500

    working on file upload

--- submit.php
+++ submit.php
@@ -61,10 +61,11 @@ if(isset($_FILES["fileToUpload"])) {
   echo 'File detected';
   $target_dir = "thumbnails".$resourceid;
   $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+  $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
   if ($_FILES["fileToUpload"]["size"] > 500000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;}
-  if($imageFileType != "JPEG" && $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+  if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
     && $imageFileType != "gif" ) {
     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
     $uploadOk = 0;}
