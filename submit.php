f50ca0f0ceafb4db45de6d8cb38016ca98cf6773
sbayegan <saeidjobs@gmail.com>
2016-03-03 18:32:04 -0500

    working on image upload

--- submit.php
+++ submit.php
@@ -84,10 +84,10 @@ if(isset($_FILES["fileToUpload"])) {
     echo "Sorry, your file was not uploaded.";
     // if everything is ok, try to upload file
     } else {
-      echo move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
+      echo move_uploaded_file($_FILES["fileToUpload"]["name"], $target_file);
 
 
-      if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+      if (move_uploaded_file($_FILES["fileToUpload"]["name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";} 
       else {
         echo "Sorry, there was an error uploading your file.";
