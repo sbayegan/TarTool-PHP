ee8b65116ff1ce924edebe84c9893d41cbac2e1c
sbayegan <saeidjobs@gmail.com>
2016-03-05 17:39:46 -0500

    working on the logout script

--- JS/code.js
+++ JS/code.js
@@ -822,7 +822,7 @@ var two = document.getElementById("login-password").value;
          //document.getElementById("condition").innerHTML =  xmlhttp.responseText;
 
                 if(xmlhttp.responseText == 1){
-                  window.location.replace("index.php");
+                  window.location.replace(".");
                   document.getElementById("condition").innerHTML = "good, lets go";
                 }
                 if(xmlhttp.responseText == 0){
--- logout.php
+++ logout.php
@@ -1,4 +1,4 @@
 <?php
 setcookie("junto","NULL",time());
-header( 'Location: http://www.tartool.com/index.php' );
+header( 'Location: tartool.com' );
 ?>
