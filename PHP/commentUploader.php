80352d1a9ced2ab9d1dae9e53823cb44ce9cf94d
sbayegan <saeidjobs@gmail.com>
2016-03-11 22:16:00 -0500

    working on comment posting

--- PHP/commentUploader.php
+++ PHP/commentUploader.php
@@ -6,7 +6,7 @@ if(isset($_POST['comment']) && isset($_POST['resource']) && isset($_COOKIE['junt
 $comment = mysqli_real_escape_string($conn,$_POST['comment']);
 $resource = mysqli_real_escape_string($conn,$_POST['resource']);
 $user = $_COOKIE['junto'];
-$query = "SELECT RESOURCE FROM LAST WHERE ONE=1";		// Get the available
+$query = "SELECT COMMENTS FROM LAST WHERE ONE=1";		// Get the available
 $result = $conn->query($query);
 $temp = mysqli_fetch_assoc($result);
 $commentid = $temp["COMMENTS"];
@@ -15,7 +15,7 @@ $next = $commentid + 1;
 $query = "UPDATE LAST SET COMMENTS='$next' WHERE ONE=1";
 $conn->query($query);
 
-$query = "INSERT INTO COMMENTS VALUES($commentid,$user,$resource,NOW(),$commentid)";
+$query = "INSERT INTO COMMENTS VALUES($commentid,$user,$resource,NOW(),$comment)";
 
 $conn->query($query);
 
