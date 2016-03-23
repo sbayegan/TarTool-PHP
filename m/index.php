b6ab18826fd86c01ecb006e5b426b8619a7e14f2
sbayegan <saeidjobs@gmail.com>
2016-03-22 22:47:31 -0400

    working on mobile css

--- m/index.php
+++ m/index.php
@@ -1,38 +1,42 @@
 <?php
 include('../datalogin.php');
+include('../PHP/printer.php');
 echo'
 <!DOCTYPE html>
 <html>
 <head>
+<title>tarTool</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
 <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
-<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
-</head>
-<body>
+<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>';
+echo '<link rel="stylesheet" type="text/css" href="mobileStyle.css">';
+echo '</head>';
 
-<div data-role="page" id="pageone">
-  <div data-role="panel" id="myPanel"> 
+echo '<body><div data-role="page" id="pageone">';
+
+echo  '<div data-role="panel" id="myPanel"> 
     <h2>Panel Header</h2>
     <p>You can close the panel by clicking outside the panel, pressing the Esc key or by swiping.</p>
-  </div> 
+  </div>';
 
-  <div data-role="header">
-    <h1>Page Header</h1>
-  </div>
+echo  '<div data-role="header">
+     		<a href="#myPanel" class="ui-btn ui-btn-inline ui-corner-all ui-shadow">Dashboard</a>
+  		</div>';
 
-  <div data-role="main" class="ui-content">
-    <p>Click on the button below to open the Panel.</p>
-    <a href="#myPanel" class="ui-btn ui-btn-inline ui-corner-all ui-shadow">Open Panel</a>
-  </div>
+echo  '<div data-role="main" class="ui-content">';
 
-  <div data-role="footer">
+card(22);
+card(22);
+card(22);
+echo  	 '</div>';
+
+echo  '<div data-role="footer">
     <h1>Page Footer</h1>
-  </div> 
-</div> 
+  </div> ';
 
-</body>
-</html>';
+echo '</div> </body>';
+echo '</html>';
 
 
 
