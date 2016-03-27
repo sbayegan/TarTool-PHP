cdba488d2a9b56df8e335d4a02139ccaf73ef7cb
sbayegan <saeidjobs@gmail.com>
2016-03-27 18:09:27 -0400

    working to merge both mobile and desktop view

--- index.php
+++ index.php
@@ -746,7 +746,7 @@ echo'</div>';
 echo  '<div class="flat-panel" id="flat-panel">
 <div class="middle-logo"><img src="/logo/junto_logo_solo.png" alt="logo" height="90%" width="auto"> </div>
 <div class="panel-flip" id="panel-flip" onclick="flipview()"><img src="/pictures/flip.png" alt="logo" height="90%" width="auto"></div>
-<div class="panel-menu" >menu</div>
+<div class="panel-menu" onclick="poppanel()" >menu</div>
   </div> ';
 
 
