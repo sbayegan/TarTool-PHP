3f4bbfa4c52444face4256a3a9aa0aca61a42027
Saeid Bay <saeidbay@Saeid.local>
2016-06-19 15:23:26 -0400

    backing up the partly modified deleteResource script

--- PHP/deleteResource.php
+++ PHP/deleteResource.php
@@ -4,20 +4,22 @@ if(!isset($_COOKIE['junto'])){
 	return;
 }
 else{
+include('../datalogin.php');
 // Get the id and sanitize it.
 
 
 	$remover = $_COOKIE['junto'];
 	$ownership = "SELECT * FROM RESOURCES WHERE RESOURCEID=".$id;
-
+	
 	$owner;
 	// Check the cookie and ownership of the resource
 	if($owner == $remover){
 	// 1 - Delete the resource from the tables
-
+		$remove = "DELETE * FROM RESOURCES WHERE RESOURCEID=".$id;
 	// 2 - Delete the categories of the resource from the categories
-
+		$removeCat = "DELETE * FROM CATEGORIES WHERE RESOURCEID=".$id;
 	// 3 - Delete if from all the libraries 
+		$removeFav = "DELETE * FROM FAVOURITES WHERE RESOURCEID=".$id;
 		echo '1';
 	}// if clause
 
